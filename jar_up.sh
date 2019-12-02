1868c42f4f7ea1be926ccf36cb69ee8f53a9bf45
Kaoru Kohashigawa <dev@kaoruk.com>
2019-12-01 18:31:15 -0800

    add a shell script to easily compile the files

---
+++ jar_up.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+mkdir jars
+
+echo "Compling Cat..."
+javac com/kaoruk/Cat.java
+jar -cvf jars/Cat.1.0.0.jar com/kaoruk/Cat.class
+rm com/kaoruk/Cat.class
+
+echo "Compling Person..."
+javac -cp jars/Cat.1.0.0.jar com/kaoruk/Person.java
+jar -cvf jars/Person.1.0.0.jar com/kaoruk/Person.class
+rm com/kaoruk/Person.class
+
+echo "Compling Life..."
+javac -cp jars/Person.1.0.0.jar com/kaoruk/Life.java
+jar -cvf jars/Life.1.0.0.jar com/kaoruk/Life.class
+rm com/kaoruk/Life.class
